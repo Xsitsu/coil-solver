03b3d191b4a802b768947883625abc8764a50975
Xsitsu <jacoblocke21@live.com>
2025-01-17 16:15:41 -0800

    Make curl example work

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <curl/curl.h>
 
-int main()
+int main(void)
 {
-    std::cout << "Hello world!" << std::endl;
+    CURL *curl = nullptr;
+    CURLcode res;
+
+    curl = curl_easy_init();
+    if (curl == nullptr) return -1;
+
+    curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+    res = curl_easy_perform(curl);
+
+    if (res != CURLE_OK)
+        fprintf(stderr, "failed! : %s\n",
+            curl_easy_strerror(res));
+
+    curl_easy_cleanup(curl);
+
     return 0;
 }
